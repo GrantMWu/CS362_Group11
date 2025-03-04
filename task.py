1ed71443cf316ea9d0cc449a5a98bbac31a65007
Grant <gmwu812@gmail.com>
2025-03-04 14:37:31 -0600

    create task.py file with docstrings

---
+++ task.py
@@ -0,0 +1,41 @@
+def conv_num(num_str):
+    """
+    Convert a string into a base 10 number.
+
+    Args:
+        num_str (str): String that represents a number.
+
+    Returns:
+        int or float: Converted number if possible.
+        None if string is invalid.
+    """
+    pass
+
+
+def my_datetime(num_sec):
+    """
+    Convert number of seconds since epoch into a readable date.
+
+    Args:
+        num_sec (int): Number of seconds to convert.
+
+    Returns:
+        str: Converted date with format MM-DD-YYYY.
+    """
+    pass
+
+
+def conv_endian(num, endian='big'):
+    """
+    Converts an int to a hexadecimal number.
+
+    Args:
+        num (int): Number to convert to hexadecimal.
+        endian (str, optional): Determines output endianness.
+            Values can be either 'big' or 'little'.
+            Defaults to 'big'.
+
+    Returns:
+        str: Converted hexadecimal number in specified notation.
+    """
+    pass
