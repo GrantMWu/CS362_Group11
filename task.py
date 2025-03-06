12ca3a66e3ba3aa3234424c3a115fa44afe48d80
Grant <gmwu812@gmail.com>
2025-03-06 11:02:37 -0600

    fix reverse in place issue

--- task.py
+++ task.py
@@ -45,23 +45,25 @@ def conv_endian(num, endian='big'):
     # check if number is positive or negative
     sign = '-' if num < 0 else ''
     num = abs(num)
+    hex_bytes = []
 
-    # divide num by 16 until num = 0
-    hex_str = ''
-    while num != 0:
-        # get next hex digit
-        hex_dig = num % 16
-        hex_dig = hex_dig if hex_dig < 10 else hex_dig + 7
-        hex_dig = chr(hex_dig + 48)
+    # edge case of 0
+    if num == 0:
+        return '00'
 
-        # convert remainder into string and add to current string
-        hex_str = hex_dig + hex_str
-        num //= 16
+    # accumulate bytes into hex_bytes
+    while num > 0:
+        byte = ''
+        for i in range(2):
+            hex_dig = num % 16
+            hex_char = chr(hex_dig + 48) if hex_dig < 10 else chr(hex_dig + 55)
+            byte = hex_char + byte
+            num //= 16
 
-    # when string is two digits long, append it to hex list
-    # read hex list forwards or backwards depending on endianess
-    hex_str = sign + hex_str
-    return hex_str
+        hex_bytes.append(byte)
 
+    # adjust hex_bytes for endianess
+    if endian == 'big':
+        hex_bytes.reverse()
 
-print(conv_endian(954786, endian='big'))
+    return sign + ' '.join(hex_bytes)
