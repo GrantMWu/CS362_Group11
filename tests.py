1b2282fb13f92c9fc2686cd645ea35634b2b6529
Grant <gmwu812@gmail.com>
2025-03-07 16:43:34 -0600

    test some edge cases

--- tests.py
+++ tests.py
@@ -1,10 +1,17 @@
 import unittest
+from task import conv_endian
 
 
-class TestCase(unittest.TestCase):
+class TestConvEndian(unittest.TestCase):
+    """Test conv_endian function"""
 
     def test1(self):
-        self.assertTrue(True)
+        """Test invalid endian value"""
+        self.assertIsNone(conv_endian(954786, 'small'))
+
+    def test2(self):
+        """Test value of 0"""
+        self.assertEqual(conv_endian(0), '00')
 
 
 if __name__ == '__main__':
